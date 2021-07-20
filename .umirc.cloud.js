4cd8c5ee347ac912a304766929f093488642116c
金建业 <jianye.jin@hand-china.com>
2021-07-20 12:00:59 +0800

    [IMP] script命令更新

--- .umirc.cloud.js
+++ .umirc.cloud.js
@@ -1,27 +1,6 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  base: 'awesome',
-  nodeModulesTransform: {
-    type: 'none',
-  },
-  mfsu: {},
-  dynamicImport: {},
-  fastRefresh: {},
-  routes: [{
-    path: '/',
-    component: '@/layouts/index',
-    routes: [
-      { path: '/', component: '@/pages/index' },
-      { path: '/home', component: '@/pages/home/index' },
-      { path: '/me', component: '@/pages/me/index' },
-      { path: '/inject', component: '@/pages/inject/index' },
-      { path: '/content-download', component: '@/pages/content-download/index' },
-      { path: '/ggeditor', component: '@/pages/ggeditor/index' },
-    ]
-  }],
-  alias: {
-    // '@components': '@/components/*',
-  },
-  fastRefresh: {},
+  publicPath: '/awesome/',
+  hash: true,
 });
