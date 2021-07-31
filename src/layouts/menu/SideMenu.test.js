9cff91c36946a5c31881a677e19008a1f52711bd
金建业 <jianye.jin@hand-china.com>
2021-07-31 10:58:29 +0800

    [IMP] 解决jest测试依赖的问题

--- src/layouts/menu/SideMenu.test.js
+++ src/layouts/menu/SideMenu.test.js
@@ -6,18 +6,13 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 Enzyme.configure({ adapter: new Adapter() });
 const { shallow, mount, render } = Enzyme;
 
-// function mockUmi() {
-//   const original = jest.requireActual('umi');
-//   return {
-//     ...original,
-//     useSelector: (callback) => {
-//       return callback;
-//     },
-//   };
-// }
-// jest.mock('umi', () => mockUmi());
-
-test(`组件SideMenu应该有1个子元素`, (done) => {
-  expect(shallow(<SideMenu />).children()).toHaveLength(1);
-  done();
+jest.mock('umi', () => {
+  return {
+    useRequest: (callback) => {
+      return callback;
+    },
+  };
+});
+test(`组件SideMenu应该有1个子元素`, () => {
+  expect(shallow(<SideMenu />)).toHaveLength(1);
 });
