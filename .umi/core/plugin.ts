452822cf89ff23b4138c1245a4db0b98f14d6e53
23726 <jianye.jin@hand-china.com>
2022-08-31 02:38:15 +0800

    1.0.7

---
+++ .umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/stevenking/workspace/awesome/2021/awesome/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','request',],
+});
+
+export { plugin };
